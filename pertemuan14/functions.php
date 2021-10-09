48638c0bb8a0a426895ec79cd5b080f2ea0dea53
rubenhidayat <rubenhidayat02@gmail.com>
2021-10-09 23:26:27 +0700

    registrasi

--- pertemuan14/functions.php
+++ pertemuan14/functions.php
@@ -130,3 +130,37 @@ function cari($keyword)
 	";
 	return query($query);
 }
+
+
+function registrasi($data)
+{
+	global $conn;
+
+	$username = stripslashes(strtolower($data['username']));
+	$password = mysqli_real_escape_string($conn, $data['password']);
+	$password2 = mysqli_real_escape_string($conn, $data['password2']);
+
+	//cek username sudah ada atau belum
+	$result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username' ");
+
+	if (mysqli_fetch_assoc($result)) {
+		echo "<script>
+                alert('username sudah digunakan');
+            </script>";
+		return false;
+	}
+
+	//cek konfirmasi password
+	if ($password2 !== $password) {
+		echo "<script>
+                alert('pastikan kedua password sama');
+            </script>";
+		return false;
+	}
+	//enkripsi password
+	$password = password_hash($password, PASSWORD_DEFAULT);
+
+	//tambahkan userbaru ke database
+	mysqli_query($conn, "INSERT INTO user VALUES('', '$username', '$password')");
+	return mysqli_affected_rows($conn);
+}
